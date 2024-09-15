ac4a880e6d847c63a9ea047dc1c35093b06ce9b9
Armandeep Singh <armandeepsingh@arman.local>
2024-09-15 19:16:30 +0530

    Uploaded BloodBankApp to git

---
+++ models/inventoryModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema(
+  {
+    inventoryType: {
+      type: String,
+      required: [true, "inventory type require"],
+      enum: ["in", "out"],
+    },
+    bloodGroup: {
+      type: String,
+      required: [true, "blood group is require"],
+      enum: ["O+", "O-", "AB+", "AB-", "A+", "A-", "B+", "B-"],
+    },
+    email: {
+      type: String,
+      required: [true, "Donar Email is Required"],
+    },
+    quantity: {
+      type: Number,
+      require: [true, "blood quanity is require"],
+    },
+    disease: {
+      type: [String],
+      required : [true , "Is there any disease?"],
+      default: ["HIV" , "Diabetes" , "Asthma"],
+    },
+    organisation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "organisation is require"],
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventoryType === "out";
+      },
+    },
+    donar: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventoryType === "in";
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Inventory", inventorySchema);
